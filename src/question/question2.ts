f4c331ca5babb051165ad5385dac964c20e28051
yuta-jeng <yuta.jeng@gmail.com>
2021-08-12 10:00:52 +0900

    add question2 type3

--- src/question/question2.ts
+++ src/question/question2.ts
@@ -32,35 +32,76 @@ export default function question2(): void {
     //         })
     //     }
 
-    //type2 //他の方法がないか模索中
+    // //type2  mapで配列を変える
 
-    const num = 200
+    // const num = 200
 
-    //クラス
-    class Multiplier {
-        private multiple: number
-        private output: string
+    // //クラス
+    // class Multiplier {
+    //     private multiple: number
+    //     private output: string
 
-        constructor(multiple: number, output: string) {
-            this.multiple = multiple
-            this.output = output
-        }
+    //     constructor(multiple: number, output: string) {
+    //         this.multiple = multiple
+    //         this.output = output
+    //     }
 
-        checkMultiple = (array: string[]): string[] => {
-            return array.map((value, index) => ((index + 1) % this.multiple === 0 ? (value = this.output) : value))
-        }
+    //     checkMultiple = (array: string[]): string[] => {
+    //         return array.map((value, index) => ((index + 1) % this.multiple === 0 ? (value = this.output) : value))
+    //     }
+    // }
+
+    // const mult1 = new Multiplier(3, 'F')
+    // const mult2 = new Multiplier(5, 'B')
+    // const mult3 = new Multiplier(15, 'FB')
+
+    // const questionNo2 = (): void => {
+    //     let numArray: string[] = [...Array(num)].map((_, i) => (i + 1).toString())
+    //     numArray = mult1.checkMultiple(numArray)
+    //     numArray = mult2.checkMultiple(numArray)
+    //     numArray = mult3.checkMultiple(numArray)
+    //     console.log(numArray)
+    // }
+
+    // tyep3    再帰関数で繰り返す
+
+    const num = 100
+
+    //エイリアス
+    type Multiplier = {
+        multiple: number
+        output: string
     }
 
-    const mult1 = new Multiplier(3, 'F')
-    const mult2 = new Multiplier(5, 'B')
-    const mult3 = new Multiplier(15, 'FB')
+    const malt1: Multiplier = {
+        multiple: 3,
+        output: 'F',
+    }
+
+    const malt2: Multiplier = {
+        multiple: 5,
+        output: 'B',
+    }
+
+    const malt3: Multiplier = {
+        multiple: 15,
+        output: 'FB',
+    }
+
+    const divide = (i: number, max: number) => {
+        i++
+        i % malt3.multiple === 0
+            ? console.log(malt3.output)
+            : i % malt2.multiple === 0
+            ? console.log(malt2.output)
+            : i % malt1.multiple === 0
+            ? console.log(malt1.output)
+            : console.log(i)
+        i < max ? divide(i, max) : null
+    }
 
     const questionNo2 = (): void => {
-        let numArray: string[] = [...Array(num)].map((_, i) => (i + 1).toString())
-        numArray = mult1.checkMultiple(numArray)
-        numArray = mult2.checkMultiple(numArray)
-        numArray = mult3.checkMultiple(numArray)
-        console.log(numArray)
+        divide(0, num)
     }
 
     questionNo2()
